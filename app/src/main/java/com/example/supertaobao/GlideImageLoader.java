368b336d37c72b0de9acaccfb9a8ecba4b9c9227
wenchao <860694896@qq.com>
2019-08-20 23:18:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/supertaobao/GlideImageLoader.java
@@ -0,0 +1,19 @@
+package com.example.supertaobao;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * @author zhang
+ */
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context)
+                .load(path)
+                .into(imageView);
+    }
+}
